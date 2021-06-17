3e58578a2c7e643e61e7f04b586f1843eba70d19
François BANITZ <francois.banitz@gmail.com>
2021-06-17 13:50:20 +0200

    core gameplay

--- lib/clicker.dart
+++ lib/clicker.dart
@@ -8,10 +8,45 @@ class Clicker extends StatefulWidget {
 }
 
 class _ClickerState extends State<Clicker> {
+  var _score = 0;
+  var _game = false;
+
+  _plus_one(){
+    setState(() {
+      _score ++;
+    });
+  }
+
+  _toggleGame(){
+    setState(() {
+      _game = ! _game;
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold( 
+      appBar: AppBar(title: Text("Clicker"),),
+      body: Column(
+        children: [
+          if (_game)
+            Column( 
+              children: [
+                Text("Score : $_score"),
+                ElevatedButton.icon(
+                  onPressed: _plus_one, 
+                  label: Text("Ajouter 1"),
+                  icon: Icon(Icons.plus_one),
+                ),
+              ],
+            ),
+          if (!_game)
+            TextButton(
+              onPressed: _toggleGame, 
+              child: Text("Démarer la partie")
+            ),
+        ],
+      ),
     );
   }
 }
